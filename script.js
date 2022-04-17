5c3d080c77f25ce69141852d6b62a3fa6060eb50
nifantov <m.v.nifantov@gmail.com>
2022-04-17 13:44:29 +0300

    add css and keydown listener

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const operatorBtn = document.querySelectorAll("[data-operator]");
 const equalBtn = document.getElementById("equal");
 const clearBtn = document.getElementById("clear");
 const deleteBtn = document.getElementById("delete");
+const plusminusBtn = document.getElementById("plusminus");
+const commaBtn = document.getElementById("comma");
 const display = document.getElementById("result");
 
 
@@ -30,6 +32,8 @@ const operate = (operator, x, y) => {
 };
 
 //button listeners
+
+window.addEventListener("keydown", keyListener);
 numberBtn.forEach((btn) => 
     btn.addEventListener("click", () => addNumber(btn.textContent))
 );
@@ -39,17 +43,35 @@ operatorBtn.forEach((btn) =>
 );
 
 equalBtn.addEventListener("click", () => getEqual());
-
 clearBtn.addEventListener("click", () => restart());
-
 deleteBtn.addEventListener("click", () => delNumber());
+plusminusBtn.addEventListener("click", () => plusminus());
+commaBtn.addEventListener("click", () => addComma());
 
-//
-const addNumber = ((number) => {
+//key listener
+function keyListener(e) {
+    if (e.key >= 0 && e.key <= 9) addNumber(e.key);
+    if (e.key === ".") addComma();
+    if (e.key === "=" || e.key === "Enter") getEqual();
+    if (e.key === "Backspace" || e.key === "Delete") delNumber();
+    if (e.key === "Escape") restart();
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') setOperator(convertOperator(e.key));
+  };
+
+function convertOperator(operator) {
+    if (operator === '/') return '÷'
+    if (operator === '*') return 'x'
+    if (operator=== '-') return '-'
+    if (operator === '+') return '+'
+};
+
+//add a number to display
+function addNumber(number) {
     if (display.textContent === "0" || clearDisplay) clear();
     display.textContent += number; 
-});    
+};
 
+//get a operator
 const setOperator = ((operator) => {
     if (currOperator != 0) getEqual ();
     currOperator = operator;
@@ -58,9 +80,9 @@ const setOperator = ((operator) => {
 });
 
 //delete 
-
 const delNumber = () => {
     display.textContent = display.textContent.slice(0, -1);
+    if (display.textContent === "") display.textContent = "0"; 
 };
 
 //clear screen
@@ -81,14 +103,34 @@ const restart = () => {
 //equal
 const getEqual = () => {
     if (currOperator === 0 || clearDisplay) return;
-    
+    if (currOperator === "÷" && display.textContent === "0") {
+        alert("OOPS! You can't devide by zero");
+        return;
+    } 
     secondNumber = display.textContent;
-    console.log(secondNumber);
-    display.textContent = operate(currOperator, firstNumber, secondNumber);
+    display.textContent = roundResult(operate(currOperator, firstNumber, secondNumber));
     clearDisplay = true;
     currOperator = 0;
 };
 
+//change
+const plusminus = () => {
+    return display.textContent = - Number(display.textContent);
+};
+
+//add comma to number
+const addComma = () => {
+    if (clearDisplay) clear();
+    if (display.textContent === "") display.textContent === "0";
+    if (display.textContent.includes(".")) return;
+    display.textContent += ".";
+};
+
+function roundResult(number) {
+    return (Math.round(number * 1000) / 1000);
+};
+
 restart();
+
     
 
